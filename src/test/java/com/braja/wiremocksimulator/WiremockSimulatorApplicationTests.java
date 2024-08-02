7ab97cfd120f1cc772c604d96c68565c638e6cb2
Jaka Brajadenta <79687223+jakabrajadenta@users.noreply.github.com>
2024-08-02 13:55:34 +0700

    initiate project

---
+++ src/test/java/com/braja/wiremocksimulator/WiremockSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braja.wiremocksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
